f35f658c2af795af02e7a7d1396968a98cdae8e7
sitkialagoz <stkalagoz@gmail.com>
2025-05-16 11:44:17 +0300

    first commit created a spring boot app

---
+++ src/main/java/com/example/grpc_poc/GrpcPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpc_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grpc_poc/GrpcPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpc_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
