c6c16ce1222600984dd7579f970a156a786e8eb2
sitkialagoz <stkalagoz@gmail.com>
2025-05-19 00:27:53 +0300

    certificate was added

---
+++ grpcclient/src/main/java/com/example/grpcclient/config/GrpcClientConfig.java
@@ -0,0 +1,31 @@
+package com.example.grpcclient.config;
+
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
+import net.devh.boot.grpc.client.channelfactory.GrpcChannelConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+
+@Configuration
+public class GrpcClientConfig {
+
+    @Bean
+    public GrpcChannelConfigurer trustCertsConfigurer() {
+        return (channelBuilder, name) -> {
+            try {
+                File certFile = new ClassPathResource("certs/ca.crt").getFile();
+                SslContext sslContext = GrpcSslContexts.forClient()
+                        .trustManager(certFile)
+                        .build();
+                ((NettyChannelBuilder) channelBuilder).sslContext(sslContext);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load TLS cert", e);
+            }
+        };
+    }
+}
